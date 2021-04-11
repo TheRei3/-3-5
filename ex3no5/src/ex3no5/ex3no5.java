44155651efca63cff9be7a3564e7a589fa3d3ebd
admin <admin@DESKTOP-8L2IG92>
2021-04-12 06:37:10 +0900

    ex3no5

---
+++ ex3no5/src/ex3no5/ex3no5.java
@@ -0,0 +1,14 @@
+package ex3no5;
+
+public class ex3no5 {   //97p
+   public static void main(String[] args) {
+	  int a = 10;
+	  int b = 4;
+	  
+	  System.out.printf("%d + %d = %d%n", a, b, a+b);
+	  System.out.printf("%d - %d = %d%n", a, b, a-b);
+	  System.out.printf("%d * %d = %d%n", a, b, a*b);
+	  System.out.printf("%d / %d = %d%n", a, b, a/b);
+	  System.out.printf("%d / %f = %f%n", a, (float)b, a/(float)b);
+    } 
+}
